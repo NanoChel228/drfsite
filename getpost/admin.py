ae714e4e341dd0012915a496bf3d6da938095a27
NanoChel228 <nano.chel228@gmail.com>
2025-12-24 21:03:40 +0500

    изменил view, не использовал generics, добавил post метод

--- getpost/admin.py
+++ getpost/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Post
 
+
+class SlugAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+
 # Register your models here.
-admin.site.register(Post)
+admin.site.register(Post, SlugAdmin)
--- getpost/views.py
+++ getpost/views.py
@@ -1,10 +1,29 @@
+from django.forms import model_to_dict
+from rest_framework.response import Response
 from rest_framework import generics
 from django.shortcuts import render
+from rest_framework.views import APIView
 
 from .serializers import PostSerializer
 from .models import Post
 
 # Create your views here.
-class getPostAPI(generics.ListAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
+
+class getPostAPI(APIView):
+    def get(self, request):
+        posts = Post.objects.all().values()
+
+        return Response({"posts": list(posts)})
+    
+    def post(self, request):
+        post_new = Post.objects.create(
+            title=request.data["title"],
+            text=request.data["text"],
+        )
+        return Response({"post": model_to_dict(post_new)})
+
+
+
+# class getPostAPI(generics.ListAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
